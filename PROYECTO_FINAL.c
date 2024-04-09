343aa8ea95af0c271eae5bdb3079e8d8b1523604
Angus19 <diego.alejocnt@uanl.edu.mx>
2024-04-09 11:13:14 -0600

    Nuevos cambios

--- PROYECTO_FINAL.c
+++ PROYECTO_FINAL.c
@@ -68,12 +68,20 @@ struct Conjunto_Datos
 
 };
 
+struct Files_Requeridos
+{
+    FILE *file_articulos;
+    FILE *file_clientes;
+    FILE *file_proveedor;
+    FILE *file_empleados;
+};
+
 //FUNCIONES PRINCIPALES
 
-void capturar_articulos(FILE *, struct Datos_Articulos *, int *, const char *);
-void capturar_empleados(FILE *, struct Datos_Empleados *, int *, const char *);
-void capturar_clientes(FILE *, struct Datos_Clientes *, int *, const char *);
-void capturar_proveedores(FILE *, struct Datos_Proveedores *, int *, const char *);
+void capturar_articulos(struct Files_Requeridos *, struct Conjunto_Datos *, int *, const char *);
+void capturar_empleados(struct Files_Requeridos *, struct Conjunto_Datos *, int *, const char *);
+void capturar_clientes(struct Files_Requeridos *, struct Conjunto_Datos *, int *, const char *);
+void capturar_proveedores(struct Files_Requeridos *, struct Conjunto_Datos *, int *, const char *);
 void controlar_ventas();
 void controlar_compras();
 
@@ -82,6 +90,8 @@ void controlar_compras();
 void convertir_cadena_a_minuscula(char *);
 bool verificar_existencia_claves(FILE *, struct Conjunto_Datos *, int *);
 bool verificar_datos_existencia(FILE *, struct Contador_Datos *, int *, int *);
+bool verificar_existencia_proveedor(FILE *, struct Datos_Proveedores *, int *, const char *);
+bool buscar_proveedores(FILE *, struct Datos_Proveedores *, int *, const char *);
 bool validar_cadenas(char *);
 
 
@@ -94,11 +104,8 @@ void pausar_terminal();
 
 int main(void)
 {
-    FILE *file_articulos, *file_proveedor, *file_clientes, *file_empleados;
-    struct Datos_Clientes clientes;
-    struct Datos_Empleados empleados;
-    struct Datos_Proveedores proveedores;
-    struct Datos_Articulos articulos;
+    struct Files_Requeridos files;
+    struct Conjunto_Datos datos_struct;
     struct Contador_Datos datos;
     char *ruta_file_articulos, *ruta_file_empleados, *ruta_file_clientes, *ruta_file_proveedores;
     int total_articulos = 0, total_clientes = 0, total_proveedores = 0, total_empleados = 0;
@@ -123,24 +130,24 @@ int main(void)
     getcwd( ruta_file_proveedores, sizeof( ruta_file_proveedores ) * 1000);
     strcat( ruta_file_proveedores, "/Files_muebleria/proveedores.dat");
 
-    file_articulos =  fopen( ruta_file_articulos, "rb" );
-    file_clientes = fopen( ruta_file_clientes, "rb" );
-    file_empleados = fopen( ruta_file_empleados, "rb" );
-    file_proveedor = fopen( ruta_file_proveedores, "rb" );
+    files.file_articulos =  fopen( ruta_file_articulos, "rb" );
+    files.file_clientes = fopen( ruta_file_clientes, "rb" );
+    files.file_empleados = fopen( ruta_file_empleados, "rb" );
+    files.file_proveedor = fopen( ruta_file_proveedores, "rb" );
 
-    if (file_articulos == NULL || 
-        file_clientes == NULL || 
-        file_empleados == NULL || 
-        file_proveedor == NULL)
+    if (files.file_articulos == NULL || 
+        files.file_clientes == NULL || 
+        files.file_empleados == NULL || 
+        files.file_proveedor == NULL)
     
         fprintf(stderr, "Error de apertura con los archivos\n");
     
     else
     {
-        fclose(file_articulos);
-        fclose(file_clientes);
-        fclose(file_empleados);
-        fclose(file_proveedor);
+        fclose(files.file_articulos);
+        fclose(files.file_clientes);
+        fclose(files.file_empleados);
+        fclose(files.file_proveedor);
 
         datos.articulos_neto = &total_articulos;
         datos.clientes_neto = &total_clientes;
@@ -154,7 +161,7 @@ int main(void)
                 limpiar_terminal();
 
                 puts("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
-                printf("\n%45s\n\n", "MENÚ DE OPCIONES");
+                printf("\n* *%45s%28s\n\n", "MENÚ DE OPCIONES", "* *");
                 puts("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
                 puts("1) Articulos");
                 puts("2) Clientes");
@@ -180,9 +187,9 @@ int main(void)
             switch (opcion)
             {
                 case 1:
-                    if (total_articulos < 100)
+                    if (total_articulos < 100 && verificar_existencia_proveedor(files.file_proveedor, &datos_struct.data_proveedores, datos.proveedores_neto, ruta_file_proveedores))
                     {
-                        capturar_articulos(file_articulos, &articulos, &total_articulos, ruta_file_articulos);
+                        capturar_articulos(&files, &datos_struct, &total_articulos, ruta_file_articulos);
                     }
                     else
                     {
@@ -193,7 +200,7 @@ int main(void)
                 case 2:
                     if (false)
                     {
-                        capturar_clientes(file_clientes, &clientes, &total_clientes, ruta_file_clientes);
+                        capturar_clientes(&files, &datos_struct, &total_clientes, ruta_file_clientes);
                     }
                     else
                     {
@@ -204,7 +211,7 @@ int main(void)
                 case 3:
                     if (total_empleados < 20)
                     {
-                        capturar_empleados(file_empleados, &empleados, &total_empleados, ruta_file_empleados);
+                        capturar_empleados(&files, &datos_struct, &total_empleados, ruta_file_empleados);
                     }
                     else
                     {
@@ -215,7 +222,7 @@ int main(void)
                 case 4:
                     if (false)
                     {
-                        capturar_proveedores(file_proveedor, &proveedores, &total_proveedores, ruta_file_proveedores);
+                        capturar_proveedores(&files, &datos_struct, &total_proveedores, ruta_file_proveedores);
                     }
                     else
                     {
@@ -288,14 +295,15 @@ int main(void)
 
 }
 
-void capturar_articulos(FILE *archivo_articulos, struct Datos_Articulos *data_articulos, int *articulos_registrados, const char *ruta_articulos_file)
+void capturar_articulos(struct Files_Requeridos *archivos, struct Conjunto_Datos *data, int *articulos_registrados, const char *ruta_articulos_file)
 {
     char respuesta[3];
     bool descripcion_correcta;
+    int i;
 
-    archivo_articulos = fopen(ruta_articulos_file, "rb+");
+    archivos->file_articulos = fopen(ruta_articulos_file, "rb+");
 
-    if (archivo_articulos == NULL)
+    if (archivos->file_articulos == NULL)
     
         fprintf(stderr, "ERROR: NO SE PUDO ABRIR CORRECTAMENTE EL ARCHIVO DE ARTICULOS");
     
@@ -329,13 +337,13 @@ void capturar_articulos(FILE *archivo_articulos, struct Datos_Articulos *data_ar
 
                     printf("Número de artículo: ");
                     limpiar_buffer_STDIN();
-                } while (scanf("%d", &data_articulos->numero_articulo) != 1);
+                } while (scanf("%d", &data->data_articulos.numero_articulo) != 1);
                 
-                if (data_articulos->numero_articulo <= 0 || data_articulos->numero_articulo > 100)
+                if (data->data_articulos.numero_articulo <= 0 || data->data_articulos.numero_articulo > 100)
                 
                     validar_errores_por_SO();
                 
-            } while (data_articulos->numero_articulo <= 0);
+            } while (data->data_articulos.numero_articulo <= 0);
 
             do
             {
@@ -345,15 +353,15 @@ void capturar_articulos(FILE *archivo_articulos, struct Datos_Articulos *data_ar
 
                 printf("Descripción del artículo: ");
                 limpiar_buffer_STDIN();
-                fgets(data_articulos->descripcion_articulo, 200, stdin);
+                fgets(data->data_articulos.descripcion_articulo, 200, stdin);
 
-                data_articulos->descripcion_articulo[strcspn(data_articulos->descripcion_articulo, "\n")] = '\0';
+                data->data_articulos.descripcion_articulo[strcspn(data->data_articulos.descripcion_articulo, "\n")] = '\0';
 
-                if (strlen(data_articulos->descripcion_articulo) > 0)
+                if (strlen(data->data_articulos.descripcion_articulo) > 0)
                 
-                    descripcion_correcta = validar_cadenas(data_articulos->descripcion_articulo);
+                    descripcion_correcta = validar_cadenas(data->data_articulos.descripcion_articulo);
 
-            } while (strlen(data_articulos->descripcion_articulo) == 0 || !descripcion_correcta);
+            } while (strlen(data->data_articulos.descripcion_articulo) == 0 || !descripcion_correcta);
 
             do
             {
@@ -363,18 +371,34 @@ void capturar_articulos(FILE *archivo_articulos, struct Datos_Articulos *data_ar
 
                     printf("Agrega un punto de reorden: ");
                     limpiar_buffer_STDIN();
-                } while (scanf("%d", &data_articulos->punto_reorden) != 1);
+                } while (scanf("%d", &data->data_articulos.punto_reorden) != 1);
                 
-                if (data_articulos->punto_reorden <= 0)
+                if (data->data_articulos.punto_reorden <= 0)
                 
                     validar_errores_por_SO();
                 
-            } while (data_articulos->punto_reorden <= 0);
+            } while (data->data_articulos.punto_reorden <= 0);
             
+            //iterar 10?
+            do
+            {
+                do
+                {
+                    limpiar_terminal();
+
+                    printf("Cuántos hay en existencia en el inventario?: ");
+                    limpiar_buffer_STDIN();
+                } while (scanf("%ld", &data->data_articulos.inventario) != 1);
+                
+                if (data->data_articulos.inventario < 0)
+                
+                    validar_errores_por_SO();
+                
+            } while (data->data_articulos.inventario < 0);
             
-            
-            *articulos_registrados++;
         }
+
+        fclose(archivos->file_articulos);
     }
     
     
@@ -382,14 +406,14 @@ void capturar_articulos(FILE *archivo_articulos, struct Datos_Articulos *data_ar
 
 }
 
-void capturar_clientes(FILE *archivo_clientes, struct Datos_Clientes *data_clientes, int *clientes_registrados, const char *ruta_clientes_file)
+void capturar_clientes(struct Files_Requeridos *archivos, struct Conjunto_Datos *data, int *clientes_registrados, const char *ruta_clientes_file)
 {
 
 
 
 }
 
-void capturar_empleados(FILE *archivo_empleados, struct Datos_Empleados *data_empleados, int *empleados_registrados, const char *ruta_empleados_file)
+void capturar_empleados(struct Files_Requeridos *archivos, struct Conjunto_Datos *data, int *empleados_registrados, const char *ruta_empleados_file)
 {
     
 
@@ -397,7 +421,7 @@ void capturar_empleados(FILE *archivo_empleados, struct Datos_Empleados *data_em
 }
 
 
-void capturar_proveedores(FILE *archivo_proveedores, struct Datos_Proveedores *data_proveedores, int *proveedores_registrados, const char *ruta_proveedores_file)
+void capturar_proveedores(struct Files_Requeridos *archivos, struct Conjunto_Datos *data, int *proveedores_registrados, const char *ruta_proveedores_file)
 {
 
 
@@ -434,6 +458,41 @@ bool verificar_datos_existencia(FILE *, struct Contador_Datos *, int *, int *)
 
 
 
+}
+
+
+bool verificar_existencia_proveedor(FILE *archivo_proveedores, struct Datos_Proveedores *data, int *proveedores_totales, const char *directorio_proveedores)
+{
+    archivo_proveedores = fopen(directorio_proveedores, "rb");
+
+    if (archivo_proveedores == NULL)
+    {
+        fprintf(stderr, "ERROR! ARCHIVO PROVEEDORES NO ES ACCESIBLE DE MOMENTO. . .");
+        return false;
+    }
+    else
+    {
+        int contador = 0;
+
+        rewind(archivo_proveedores);
+
+        while (fread(data, sizeof(struct Datos_Proveedores), 1, archivo_proveedores) == 1)
+        {
+            if (data->numero_proveedor != 0)
+            {
+                contador++;
+                *proveedores_totales = contador;
+            }
+        }
+        
+        if (*proveedores_totales == 0)
+        {
+            printf("NO HAY PROVEEDORES DE ARTÍCULOS, EN EXISTENCIA. . .");
+            return false;
+        }
+        return true;
+    }
+    
 }
 
 bool validar_cadenas(char *caracter)
